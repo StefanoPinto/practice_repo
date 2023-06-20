87ea691dc494a416568ea466a96cdb48190a3aed
Stefano Pinto <stefanopinto@hotmail.de>
2023-06-20 20:15:19 +0200

    create body mass heatmap

--- main_script.R
+++ main_script.R
@@ -3,4 +3,18 @@ library(palmerpenguins)
 library(janitor)
 
 
-  
+penguins_clean <- penguins %>% 
+  clean_names() %>% 
+  na.omit() %>% 
+  map_at(.at = c("species", "island"), .f = ~tolower(.x)) %>% 
+  as_tibble()
+
+
+penguins_clean %>% 
+  select(species, island, body_mass_g) %>% 
+  group_by(species, island) %>% 
+  summarize(mean_mass = mean(body_mass_g)) %>% 
+  ggplot(aes(species, island, fill = mean_mass)) +
+  geom_tile(col = "white") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "bottom")
