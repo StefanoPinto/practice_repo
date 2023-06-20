63c8cd0c3ff1a4d097c9235aa7df310d05b546ce
Stefano Pinto <stefanopinto@hotmail.de>
2023-06-20 20:08:40 +0200

    clean the penguins dataframe

--- main_script.R
+++ main_script.R
@@ -1,3 +1,9 @@
 library(tidyverse)
 library(palmerpenguins)
+library(janitor)
 
+penguins_clean <- penguins %>% 
+  clean_names() %>% 
+  na.omit() %>% 
+  map_at(.at = c("species", "island"), .f = ~tolower(.x)) %>% 
+  as_tibble()
