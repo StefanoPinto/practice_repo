ceadf870cddf96d056448bfd8c07e72c63a41d82
Stefano Pinto <stefanopinto@hotmail.de>
2023-06-20 20:22:10 +0200

    more comments

--- main_script.R
+++ main_script.R
@@ -3,14 +3,14 @@ library(palmerpenguins)
 library(janitor)
 
 
+# Cleaning
 penguins_clean <- penguins %>% 
   clean_names() %>% 
   na.omit() %>% 
   map_at(.at = c("species", "island"), .f = ~tolower(.x)) %>% 
   as_tibble()
-  
 
-
+# Visuals  
 penguins_clean %>% 
   select(species, island, body_mass_g) %>% 
   group_by(species, island) %>% 
@@ -20,3 +20,6 @@ penguins_clean %>%
   theme_bw(base_size = 14) +
   theme(legend.position = "bottom")
 
+
+# Created a "cleaning" branch and cleaned the data. Guess this branch
+# can be deleted now
