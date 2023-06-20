b488c61781a5afed07f228b464f4b6f83c7ea31d
Stefano Pinto <stefanopinto@hotmail.de>
2023-06-20 20:12:52 +0200

    idk

--- main_script.R
+++ main_script.R
@@ -3,4 +3,8 @@ library(palmerpenguins)
 library(janitor)
 
 
-  
+penguins_clean <- penguins %>% 
+  clean_names() %>% 
+  na.omit() %>% 
+  map_at(.at = c("species", "island"), .f = ~tolower(.x)) %>% 
+  as_tibble()
